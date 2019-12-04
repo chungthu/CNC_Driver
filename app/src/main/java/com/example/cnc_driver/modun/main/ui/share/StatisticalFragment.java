84528bcaaa9006f1e2c37310be5d999390259511
DiamondTran <tranvankimcuong@gmail.com>
2019-12-04 21:48:29 +0700

    update continue

--- app/src/main/java/com/example/cnc_driver/modun/main/ui/share/StatisticalFragment.java
+++ app/src/main/java/com/example/cnc_driver/modun/main/ui/share/StatisticalFragment.java
@@ -1,10 +1,14 @@
 package com.example.cnc_driver.modun.main.ui.share;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +17,51 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cnc_driver.R;
+import com.example.cnc_driver.common.base.BaseFragment;
 
-public class StatisticalFragment extends Fragment {
+public class StatisticalFragment extends BaseFragment {
 
-    private StatisticalViewModel shareViewModel;
+private Button btnca,btndate,btnmonth;
+private TextView txtfirt,txtend;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        shareViewModel =
-                ViewModelProviders.of(this).get(StatisticalViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
-        shareViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_share;
     }
+
+    @Override
+    protected void initializeViews(View view, Bundle savedInstanceState) {
+       init();
+       btnca.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               AlertDialog.Builder mbuilder = new AlertDialog.Builder(getActivity());
+               View mview= getLayoutInflater().inflate(R.layout.dialog_end,null);
+               EditText editText=(EditText) mview.findViewById(R.id.edtnumber);
+               Button btn=(Button) mview.findViewById(R.id.btnkethuc);
+
+               mbuilder.setView(mview);
+               AlertDialog alertDialog= mbuilder.create();
+               alertDialog.show();
+               btn.setOnClickListener(new View.OnClickListener() {
+                   @Override
+                   public void onClick(View view) {
+                       txtfirt.setText(editText.getText());
+                  alertDialog.dismiss();
+                   }
+               });
+           }
+       });
+
+    }
+
+    private void init() {
+        btnca= getView().findViewById(R.id.btnend);
+      btndate=getView().findViewById(R.id.btndate);
+      btnmonth= getView().findViewById(R.id.btnmonth);
+      txtfirt= getView().findViewById(R.id.txtfirt);
+      txtend = getView().findViewById(R.id.txtend);
+    }
+
+
 }
